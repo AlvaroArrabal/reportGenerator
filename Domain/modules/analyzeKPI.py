9db86e3593271cb09ef1964f94b8e8295ab5a402
Alvaro <arrabalalvaro@gmail.com>
2023-01-11 16:41:39 +0100

    starting new proyect structure

---
+++ Domain/modules/analyzeKPI.py
@@ -0,0 +1,40 @@
+
+
+def CDR():
+    pass
+
+def CSSR():
+    pass
+
+def iniciated_calls():
+    pass
+
+def throughput():
+    pass
+
+def interference():
+    pass
+
+def availability():
+    pass
+
+def MIMO_rank2():
+    pass
+
+def MIMO_rank4():
+    pass
+
+def CSFB():
+    pass
+
+def CA_pcell():
+    pass
+
+def CA_scell():
+    pass
+
+def intraLTEHosr ():
+    pass
+
+def SRVCC():
+    pass
