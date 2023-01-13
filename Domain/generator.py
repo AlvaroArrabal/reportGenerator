bb618dc06aac06599b49db053a44df49d5b55118
Alvaro <arrabalalvaro@gmail.com>
2023-01-13 14:27:18 +0100

    starting generator.py

--- Domain/generator.py
+++ Domain/generator.py
@@ -4,5 +4,25 @@ import pandas as pd
 
 data = getData.get2G("M1990E2")
 
-a = getData.getNOK(2,1)
 
+def justification(numCells,numTechs):
+    
+    # 1º getNOK
+    listNOK = getData.getNOK(numCells,numTechs)
+
+    
+    # 2º type of NOK?
+    for i in range(len(listNOK)):
+        match listNOK[i][1]:
+            case '5G RLC UL Traffic (GB)':
+                # 3º NOK_i = analyzeKPI
+                print("RSSI")
+            case 'Interference 4G PUSCH UL (RSSI UL 4G)':
+                print("PUSCH")
+        
+
+
+justification(2,2)
+
+def word():
+    pass
