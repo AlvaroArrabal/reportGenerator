9a906edb57b7319c499462237844214eb843a4c1
Alvaro <arrabalalvaro@gmail.com>
2023-01-13 14:18:57 +0100

    getNOK improved

--- Domain/modules/getData.py
+++ Domain/modules/getData.py
@@ -10,20 +10,19 @@ def getNOK(numCells,numTechs):
     columns = []
 
     for i in range(0,numCells*numTechs*2,2): 
-        columns.append([i+5, i+6,"Unnamed: " + str(i+6)])
-
+        columns.append([i+5, i+6,"Unnamed: " + str(i+5),"Unnamed: " + str(i+6)])
     columnsList = []
 
     for i in columns:
         columnsList.append(df.iloc[:,[3,i[0],i[1]]])
-        
+    
     NOKList = []
 
     for i in range(len(columnsList)):
         for j in range(len(columnsList[i])):
-            if columnsList[i].iloc[j][columns[i][2]] == "NOK":
-                NOKList.append(columnsList[i].iloc[j]["Unnamed: 3"])
-                
+            if columnsList[i].iloc[j][columns[i][3]] == "NOK": 
+                NOKList.append([columnsList[i].iloc[0,1] , columnsList[i].iloc[j]["Unnamed: 3"]])
+    return NOKList
 
 def get2G(site):
     path2g = currentPath + "\\Data\\2g.xlsx"
